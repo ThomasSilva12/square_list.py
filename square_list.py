dcf3d01698b43ef89d6dd7bc660d4c13d989ac8e
thomassilva <thomasj1204@icloud.com>
2023-11-15 17:49:10 -0800

    Initial commit

---
+++ square_list.py
@@ -0,0 +1,19 @@
+### Thomas Silva
+### ENGR 103 Assignment 7a.
+### Nov 15, 2023.
+
+def square_list(nums):
+
+    for i in range(len(nums)):
+        nums[i] = nums[i] ** 2
+
+my_list = [3, 4, 5, 6, 7]
+square_list(my_list)
+print(my_list)
+
+
+
+
+
+
+
